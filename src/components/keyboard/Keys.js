60ad665df233544bcf872cfd3f6b531820ac801e
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-04-29 10:14:44 +0100

    increment on attempts

--- src/components/keyboard/Keys.js
+++ src/components/keyboard/Keys.js
@@ -3,11 +3,14 @@ import {AppContext} from "../../App";
 
 //Register keyValue and largeKey
 function Keys({ keyValue, largeKey }) {
-    const {board, setBoard} = useContext(AppContext);
+    // Register Values for board on click function
+    const {board, setBoard, currentAttempt, setCurrentAttempt} = useContext(AppContext);
     const selectLetters = () => {
         const newBoard = [...board]
-        newBoard[0][0] = keyValue
+        newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyValue
         setBoard(newBoard);
+        //Increment on attempts
+        setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition = +1});
     }
   return (
     <div className="keys" id={largeKey && "big"} onClick={selectLetters}>
