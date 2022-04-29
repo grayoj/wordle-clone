cd1feff8f5e27a88a60efa61140ec2bbd8baaf6e
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-04-29 12:29:50 +0100

    define game logic

--- src/components/keyboard/Letters.js
+++ src/components/keyboard/Letters.js
@@ -1,13 +1,16 @@
-import React, { useContext } from "react";
-import { AppContext } from "../../App";
+import React, { useContext } from 'react';
+import { AppContext } from '../../App';
 
 function Letters({ letterPosition, attemptValue }) {
-  const { board } = useContext(AppContext);
+  const { board, correctWords, currentAttempt } = useContext(AppContext);
   const letters = board[attemptValue][letterPosition];
- 
   
+  const correct = correctWords[letterPosition] === letters
+  const nearly = !correct && letters !== "" && correctWords.includes(letters)
+  
+  const lettersState = currentAttempt.attempt > attemptValue && (correct ? "correct" : nearly ? "nearly" : "wrong");
   return (
-  <div className="letters" id={"correct"}>
+  <div className='letters' id={lettersState}>
     {""}
     {letters}
   </div>
