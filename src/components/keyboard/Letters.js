03752112bfdfdee72e81ec14604874dbe81e4ee9
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-04-29 12:06:37 +0100

    #fix letter alignment

--- src/components/keyboard/Letters.js
+++ src/components/keyboard/Letters.js
@@ -3,8 +3,15 @@ import { AppContext } from "../../App";
 
 function Letters({ letterPosition, attemptValue }) {
   const { board } = useContext(AppContext);
-  const letters = board[letterPosition][attemptValue];
-  return <div className="letters">{letters}</div>;
+  const letters = board[attemptValue][letterPosition];
+ 
+  
+  return (
+  <div className="letters" id={"correct"}>
+    {""}
+    {letters}
+  </div>
+  );
 }
 
 export default Letters;
