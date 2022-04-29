3868bd7fc2fdbcfe3b746b5a62d2196902866e60
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-04-29 10:03:34 +0100

    implemented function for onlick #2

--- src/components/keyboard/Keys.js
+++ src/components/keyboard/Keys.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
+import {AppContext} from "../../App";
 
 //Register keyValue and largeKey
 function Keys({ keyValue, largeKey }) {
+    const {board, setBoard} = useContext(AppContext);
+    const selectLetters = () => {
+        const newBoard = [...board]
+        newBoard[0][0] = keyValue
+        setBoard(newBoard);
+    }
   return (
-    <div className="keys" id={largeKey && "big"}>
+    <div className="keys" id={largeKey && "big"} onClick={selectLetters}>
       {keyValue}
     </div>
   );
